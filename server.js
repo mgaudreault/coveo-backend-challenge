04652a0fde49c23d25018714a1aed2576adae418
mgaudreault <maxime.g.gaudreault@gmail.com>
2017-03-09 15:11:54 -0500

    * fixing heroku port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import router from './api/routes'
 
 app.use(router);
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 
 var server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
